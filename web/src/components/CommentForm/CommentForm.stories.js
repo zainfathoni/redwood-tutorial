3b1fcbd8d0b85c9f83c167e0500f3cb041f78e22
Zain Fathoni <zain.fathoni@gmail.com>
2021-01-24 07:36:40 +0800

    Creating a Comment Form
    yarn rw g component CommentForm
    https://redwoodjs.com/tutorial2/creating-a-comment-form#creating-a-comment-form

---
+++ web/src/components/CommentForm/CommentForm.stories.js
@@ -0,0 +1,11 @@
+import CommentForm from './CommentForm'
+
+export const generated = () => {
+  return (
+    <div className="m-4">
+      <CommentForm />
+    </div>
+  )
+}
+
+export default { title: 'Components/CommentForm' }
