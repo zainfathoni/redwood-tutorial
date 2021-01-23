65e9bfea225c48c85fedc0634c4047c0ed5e8fe0
Zain Fathoni <zain.fathoni@gmail.com>
2021-01-23 15:07:43 +0800

    Displaying the Summary
    https://redwoodjs.com/tutorial2/our-first-story#displaying-the-summary

--- web/src/components/BlogPostsCell/BlogPostsCell.js
+++ web/src/components/BlogPostsCell/BlogPostsCell.js
@@ -23,7 +23,7 @@ export const Success = ({ posts }) => {
     <div className="-mt-10">
       {posts.map((post) => (
         <div key={post.id} className="mt-10">
-          <BlogPost post={post} />
+          <BlogPost post={post} summary={true} />
         </div>
       ))}
     </div>
