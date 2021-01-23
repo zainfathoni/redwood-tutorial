4f8aa8b1f7a20712c8b19c43eac100630273d4ec
Zain Fathoni <zain.fathoni@gmail.com>
2021-01-23 23:01:35 +0800

    Storybook
    https://redwoodjs.com/tutorial2/building-a-component-the-redwood-way#storybook

--- web/src/components/Comment/Comment.js
+++ web/src/components/Comment/Comment.js
@@ -1,8 +1,19 @@
-const Comment = () => {
+const formattedDate = (dateTime) => {
+  const parsedDate = new Date(dateTime)
+  const month = parsedDate.toLocaleString('default', { month: 'long' })
+  return `${parsedDate.getDate()} ${month} ${parsedDate.getFullYear()}`
+}
+
+const Comment = ({ comment }) => {
   return (
-    <div>
-      <h2>{'Comment'}</h2>
-      <p>{'Find me in ./web/src/components/Comment/Comment.js'}</p>
+    <div className="bg-gray-200 p-8 rounded-lg">
+      <header className="flex justify-between">
+        <h2 className="font-semibold text-gray-700">{comment.name}</h2>
+        <time className="text-xs text-gray-500" dateTime={comment.createdAt}>
+          {formattedDate(comment.createdAt)}
+        </time>
+      </header>
+      <p className="text-sm mt-2">{comment.body}</p>
     </div>
   )
 }
--- web/src/components/Comment/Comment.stories.js
+++ web/src/components/Comment/Comment.stories.js
@@ -1,7 +1,17 @@
 import Comment from './Comment'
 
 export const generated = () => {
-  return <Comment />
+  return (
+    <div className="m-4">
+      <Comment
+        comment={{
+          name: 'Zain Fathoni',
+          body: 'This is the first comment!',
+          createdAt: '2020-01-01T12:34:56Z',
+        }}
+      />
+    </div>
+  )
 }
 
 export default { title: 'Components/Comment' }
