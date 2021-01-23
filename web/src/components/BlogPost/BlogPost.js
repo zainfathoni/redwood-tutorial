b8a57ebc91265493b9dcee08903d9529aedb4175
Zain Fathoni <zain.fathoni@gmail.com>
2021-01-23 15:06:24 +0800

    Our First Story
    https://redwoodjs.com/tutorial2/our-first-story#our-first-story

--- web/src/components/BlogPost/BlogPost.js
+++ web/src/components/BlogPost/BlogPost.js
@@ -1,6 +1,8 @@
 import { Link, routes } from '@redwoodjs/router'
 
-const BlogPost = ({ post }) => {
+const truncate = (text, length) => text.substring(0, length) + '...'
+
+const BlogPost = ({ post, summary = false }) => {
   return (
     <article>
       <header>
@@ -8,7 +10,9 @@ const BlogPost = ({ post }) => {
           <Link to={routes.blogPost({ id: post.id })}>{post.title}</Link>
         </h2>
       </header>
-      <div className="mt-2 text-gray-900 font-light">{post.body}</div>
+      <div className="mt-2 text-gray-900 font-light">
+        {summary ? truncate(post.body, 100) : post.body}
+      </div>
     </article>
   )
 }
