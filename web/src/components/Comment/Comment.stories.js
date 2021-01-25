75f597704a3351873a66618317eef0b4cdab05b7
Zain Fathoni <zain.fathoni@gmail.com>
2021-01-25 23:43:35 +0800

    Mocking currentUser for Storybook
    https://redwoodjs.com/tutorial2/role-based-authorization-control-rbac#mocking-currentuser-for-storybook

--- web/src/components/Comment/Comment.stories.js
+++ web/src/components/Comment/Comment.stories.js
@@ -1,6 +1,24 @@
 import Comment from './Comment'
 
-export const generated = () => {
+export const defaultView = () => {
+  return (
+    <div className="m-4">
+      <Comment
+        comment={{
+          name: 'Zain Fathoni',
+          body: 'This is the first comment!',
+          createdAt: '2020-01-01T12:34:56Z',
+        }}
+      />
+    </div>
+  )
+}
+
+export const moderatorView = () => {
+  mockCurrentUser({
+    roles: ['moderator'],
+  })
+
   return (
     <div className="m-4">
       <Comment
