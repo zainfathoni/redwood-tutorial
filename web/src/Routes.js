9b3bcfc734f404dca480dbcd6ba894166840ef9e
Zain Fathoni <zain.fathoni@gmail.com>
2021-01-25 21:22:49 +0800

    Role-Based Authorization Control (RBAC)
    Roles in Routes
    https://redwoodjs.com/tutorial2/role-based-authorization-control-rbac

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -16,7 +16,7 @@ const Routes = () => {
       <Route path="/blog-post/{id:Int}" page={BlogPostPage} name="blogPost" />
       <Route path="/about" page={AboutPage} name="about" />
       <Route path="/" page={HomePage} name="home" />
-      <Private unauthenticated="home">
+      <Private unauthenticated="home" role="admin">
         <Route path="/admin/posts/new" page={NewPostPage} name="newPost" />
         <Route path="/admin/posts/{id:Int}/edit" page={EditPostPage} name="editPost" />
         <Route path="/admin/posts/{id:Int}" page={PostPage} name="post" />
